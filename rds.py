ff2e5e960d0f026e476b4b114feb90f5ef9cbec0
eveyang <ping187@163.com>
2019-09-16 11:22:39 +0800

    Add files via upload
    监控阿里云数据库相关

---
+++ rds.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding=utf-8
+import datetime
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkrds.request.v20140815 import DescribeResourceUsageRequest
+from aliyunsdkrds.request.v20140815 import DescribeResourceUsageRequest
+UTC_End = datetime.datetime.today() - datetime.timedelta(hours=8)
+UTC_Start = UTC_End - datetime.timedelta(minutes=25)
+
+StartTime = datetime.datetime.strftime(UTC_Start, '%Y-%m-%dT%H:%MZ')
+EndTime = datetime.datetime.strftime(UTC_End, '%Y-%m-%dT%H:%MZ')
+client = AcsClient('LTAIDZmxNTfef5Ms', 'GRbbRRKWwtS6ZuItQ55eRM25OUIj2t', 'cn-hangzhou')
+
+request = DescribeResourceUsageRequest.DescribeResourceUsageRequest()
+#request.set_accept_format('json')
+request.set_DBInstanceId('rm-bp1qtk3z3g646in7e')
+response = client.do_action_with_exception(request)
+#request.set_action_name('DescribeDBInstancePerformance')
+
+#request.set_query_params(dict(DBInstanceId="rm-bp1qtk3z3g646in7e",key="性能指标",StartTime="2018-05-22T15:00Z",EndTime="2018-05-22T16:00Z"))
+
+#print(client.do_action_with_exception(request))
+# python2:  print(response)
+print(str(response, encoding='utf-8'))
